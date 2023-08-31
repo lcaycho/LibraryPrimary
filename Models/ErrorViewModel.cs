d55a45db35e45aebb52a0a88a45140be681f9566
Leonardo Caycho <leonardo_caycho@usmp.pe>
2023-08-30 19:32:12 -0500

    Version codigo fuente

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PROYECTO_LIBRARY_PRIMARY.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
